69961c43ba166ec897e79195652b5dcee0793356
benjp009 <patin.benjamin@gmail.com>
2022-01-27 10:44:39 +0100

    fix Rockets linters 2

--- src/pages/rockets.js
+++ src/pages/rockets.js
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Rockets = ({ rocket }) => (
-    <div className='rocket'>
-      <div>
-        <img className="rocket-picture" src={rocket.flickr_images} alt={rocket.description} />
-      </div>
-      <div className="rocket-text">
-        <h2 className="rocket-title"> {rocket.rocket_name} </h2>
-        <p className="rocket-description"><span className="rocket-reserved">Reserved</span>{rocket.description}</p>
-        <button type="button" className="rocket-button">Book Button</button>
-      </div>
+  <div className='rocket'>
+    <div>
+      <img className="rocket-picture" src={rocket.flickr_images} alt={rocket.description} />
     </div>
-  );
+    <div className="rocket-text">
+      <h2 className="rocket-title"> {rocket.rocket_name} </h2>
+      <p className="rocket-description"><span className="rocket-reserved">Reserved</span>{rocket.description}</p>
+      <button type="button" className="rocket-button">Book Button</button>
+    </div>
+  </div>
+);
 
-  Rockets.propTypes = {
+  Rockets.propTypes(rocket) = {
     flickr_images: PropTypes.any,
     rocket_name: PropTypes.string,
     description: PropTypes.string,
