c7d838c2bc7bfd11cc1c2ebe301f30ac58528dbf
benjp009 <patin.benjamin@gmail.com>
2022-01-27 11:47:28 +0100

    fix react-app error

--- src/pages/rockets.js
+++ src/pages/rockets.js
@@ -2,40 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Rockets({ rocket }) {
-  <div className="rocket">
-    <div>
-      <img className="rocket-picture" src={rocket.flickr_images} alt={rocket.description} />
+  return (
+    <div className="rocket">
+      <div>
+        <img className="rocket-picture" src={rocket.flickr_images} alt={rocket.description} />
+      </div>
+      <div className="rocket-text">
+        <h2 className="rocket-title">
+          {rocket.rocket_name}
+        </h2>
+        <p className="rocket-description">
+          <span className="rocket-reserved">
+            Reserved
+          </span>
+          {rocket.description}
+        </p>
+        {rocket.reserved ? (
+          <button type="button" className="rocket-button">
+            Book Button
+          </button>
+        ) : null}
+      </div>
     </div>
-    <div className="rocket-text">
-      <h2 className="rocket-title">
-        {rocket.rocket_name}
-      </h2>
-      <p className="rocket-description">
-        <span className="rocket-reserved">
-          Reserved
-        </span>
-        {rocket.description}
-      </p>
-        <button type="button" className="rocket-button">
-          Book Button
-        </button>
+  )
 
-    </div>
-  </div>;
 }
 
 Rockets.propTypes = {
   anyProp: PropTypes.string,
 };
-
-
-<p className="rockets-desc-p">
-              {rocket.reserved ? (
-                <button
-                  type="button"
-                  className="reserved-span rocktbtn"
-                  title="button"
-                >
-                  Reserved
-                </button>
-              ) : null}
