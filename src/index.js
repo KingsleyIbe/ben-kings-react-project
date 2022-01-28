55695733908901f6342f1b902a28646641b19848
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-28 20:21:28 +0100

    Fix style errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import store from './redux/configureStore';
 
